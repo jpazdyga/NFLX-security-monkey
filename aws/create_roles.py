7eb129bd40a82619142964d13b8b8f8069e7b923
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-09-15 10:33:50 +0100

    Added 10 seconds to wait until configure Trust Relationship

--- aws/create_roles.py
+++ aws/create_roles.py
@@ -5,6 +5,7 @@ try:
 except ImportError:
  import simplejson as json
 
+import time
 import boto
 from pprint import pprint
 
@@ -88,7 +89,8 @@ SecurityMonkeyRole = iam.create_role('SecurityMonkey')
 SecurityMonkeyPolicy = iam.put_role_policy('SecurityMonkey','SecurityMonkeyReadOnly',role_policy_json)
 SecurityMonkeyUser = iam.create_user('security_monkey_docker_user')
 SecurityMonkeyPolicy = iam.put_user_policy('security_monkey_docker_user','SM_ASSUMEROLE',user_policy_json)
-#SecurityMonkeyRelationship = iam.update_assume_role_policy('SecurityMonkey',trusted_relationships_json)
+time.sleep(10)
+SecurityMonkeyRelationship = iam.update_assume_role_policy('SecurityMonkey',trusted_relationships_json)
 
 CreateAccessKeys = iam.create_access_key('security_monkey_docker_user')
 id = CreateAccessKeys.access_key_id
