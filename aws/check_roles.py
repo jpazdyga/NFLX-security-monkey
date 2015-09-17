edb6a423afa198f587b4f28335477621ac9f9b0a
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-09-17 12:22:07 +0100

    Added file containing required policies and new script to ensure they aren't changed

---
+++ aws/check_roles.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+try:
+ import json
+except ImportError:
+ import simplejson as json
+
+import time
+import sys, getopt
+import boto
+import urllib
+from jsoncompare import jsoncompare
+from policies import *
+
+def comparejson(a, b):
+    result = jsoncompare.are_same(a, b)[0]
+    if result:
+        ## JSONs are the same ##
+        print("0");
+    else:
+        ## JSONs are different ##
+        print("1");
+#        print(a);
+#        print(b);
+#        print(jsoncompare.are_same(a, b)[1])
+
+def usage():
+    print("Usage: " + sys.argv[0] + " [user-policy-name|user-policy-document|role-policy-name|role-policy-document|role-trusted]" )
+
+def securitymonkeyuserpolicyname():
+    SecurityMonkeyUserPolicy = iam.get_all_user_policies('security_monkey_docker_user').policy_names
+    print(SecurityMonkeyUserPolicy);
+
+def securitymonkeyuserpolicydump():
+    SecurityMonkeyUserPolicyDump = iam.get_user_policy('security_monkey_docker_user','SM_ASSUMEROLE').policy_document
+    updoc = urllib.unquote(SecurityMonkeyUserPolicyDump).decode('utf8');
+    a = json.loads(updoc)
+    b = json.loads(user_policy_json)
+    comparejson(a, b)
+
+def securitymonkeyrolepolicyname():
+    SecurityMonkeyRolePolicyName = iam.get_role_policy('SecurityMonkey','SecurityMonkeyReadOnly').policy_name
+    print(SecurityMonkeyRolePolicyName);
+
+def securitymonkeyrolepolicydump():
+    SecurityMonkeyRolePolicyDump = iam.get_role_policy('SecurityMonkey','SecurityMonkeyReadOnly').policy_document
+    updoc = urllib.unquote(SecurityMonkeyRolePolicyDump).decode('utf8');
+    a = json.loads(updoc)
+    b = json.loads(role_policy_json)
+    comparejson(a, b)
+
+def securitymonkeyroletrusted():
+    SecurityMonkeyRoleTrusted = iam.get_role('SecurityMonkey').assume_role_policy_document
+    updoc = urllib.unquote(SecurityMonkeyRoleTrusted).decode('utf8');
+    a = json.loads(updoc)
+    b = json.loads(trusted_relationships_json)
+    comparejson(a, b)
+
+myopts = sys.argv[1:]
+for o in myopts:
+    if o == "user-policy-name":
+        securitymonkeyuserpolicyname()
+    elif o == "user-policy-document":
+        securitymonkeyuserpolicydump()
+    elif o == "role-policy-name":
+        securitymonkeyrolepolicyname()
+    elif o == "role-policy-document":
+        securitymonkeyrolepolicydump()
+    elif o == "role-trusted":
+        securitymonkeyroletrusted()
+    elif o in 'usage|help|--help|-h':
+        usage()
+    else:
+        print("Wrong argument given.")
+        usage()
---
+++ aws/policies.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+import boto
+
+iam = boto.connect_iam()
+AccountId = iam.get_user()['get_user_response']['get_user_result']['user']['arn'].split(':')[4]
+
+trusted_relationships_json = '''{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::''' + AccountId + ''':user/security_monkey_docker_user"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+'''
+role_policy_json = '''{
+  "Statement": [
+    {
+      "Action": [
+        "cloudwatch:Describe*",
+        "cloudwatch:Get*",
+        "cloudwatch:List*",
+        "ec2:Describe*",
+        "elasticloadbalancing:Describe*",
+        "iam:List*",
+        "iam:Get*",
+        "route53:Get*",
+        "route53:List*",
+        "rds:Describe*",
+        "s3:GetBucketAcl",
+        "s3:GetBucketCORS",
+        "s3:GetBucketLocation",
+        "s3:GetBucketLogging",
+        "s3:GetBucketPolicy",
+        "s3:GetBucketVersioning",
+        "s3:GetLifecycleConfiguration",
+        "s3:ListAllMyBuckets",
+        "sdb:GetAttributes",
+        "sdb:List*",
+        "sdb:Select*",
+        "ses:Get*",
+        "ses:List*",
+        "sns:Get*",
+        "sns:List*",
+        "sqs:GetQueueAttributes",
+        "sqs:ListQueues",
+        "sqs:ReceiveMessage"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+'''
+
+user_policy_json = '''{
+  "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "Stmt1415584016000",
+        "Effect": "Allow",
+        "Action": [
+          "sts:AssumeRole"
+        ],
+        "Resource": [
+          "arn:aws:iam::''' + AccountId + ''':role/SecurityMonkey"
+        ]
+      }
+    ]
+}
+'''
