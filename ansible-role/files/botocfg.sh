6d2b753864513b53bca336ec389f941bacdfdff3
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-11-04 09:33:34 +0000

    Mover ansible module to the separate repo

--- ansible-role/files/botocfg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo -e "[Credentials]\naws_access_key_id = $AWS_ACCESS_KEY\naws_secret_access_key = $AWS_SECRET_ACCESS_KEY" > /etc/boto.cfg
--- ansible-role/files/sm_scheduler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH="/usr/local/src/security_monkey/"
-export SECURITY_MONKEY_SETTINGS="/usr/local/src/security_monkey/env-config/config-deploy.py"
-python /usr/local/src/security_monkey/manage.py start_scheduler &
